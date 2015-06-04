4e558b6b138777343f40c6c21bced444947e29af
Luca Galli <lgalli@elet.polimi.it>
2015-06-04 18:55:53 +0200

    Fixing dependencies

--- app/templates/server_express.js
+++ app/templates/server_express.js
@@ -5,7 +5,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var swaggerize = require('swaggerize-express');
 var path = require('path');
-var db = require('./lib/database');
+var db = require('./lib/lib_mongoose');
 
 var app = express();
 
@@ -23,6 +23,12 @@ app.use(swaggerize({
     api: path.resolve('./<%=apiPath%>'),
     handlers: path.resolve('./handlers')
 }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 server.listen(8000, function () {
     app.setHost(server.address().address + ':' + server.address().port);
